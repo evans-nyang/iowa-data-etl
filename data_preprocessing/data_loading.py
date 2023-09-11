6a5f027aa3330d0b08e8b4a26a72d430e3661ccb
evans-nyang <nyanggithub@gmail.com>
2023-09-12 02:37:43 +0300

    Change raw data format save to parquet

--- data_preprocessing/data_loading.py
+++ data_preprocessing/data_loading.py
@@ -1,8 +1,7 @@
 import os
 import logging
-from data_wrangling import preprocess_data, save_data
+from data_wrangling import preprocess_data, save_data_to_json, save_data_to_parquet
 from data_extraction import fetch_data
-from file_handler import save_dataset_to_json
 from config_loader import load_config 
 
 # Configure logging
@@ -27,21 +26,21 @@ def main():
         config = load_config('../config/config.yml')
 
         # Get the file paths from the loaded configuration
-        raw_filepath = os.path.join(config['file_paths']['raw'], 'raw_iowa_data.json')
+        raw_filepath = os.path.join(config['file_paths']['raw'], 'raw_iowa_data.parquet')
         clean_filepath = os.path.join(config['file_paths']['processed'], 'clean_iowa_data.json')
 
         # Fetch data from the data source
         data = fetch_data()
 
         if data:
-            # Save raw data to JSON file
-            save_dataset_to_json(data, raw_filepath)
+            # Save raw data to parquet file
+            save_data_to_parquet(data, raw_filepath)
 
             # Preprocess data
             cleaned_data = preprocess_data(data)
 
             # Save cleaned data to JSON file
-            save_data(cleaned_data, clean_filepath)
+            save_data_to_json(cleaned_data, clean_filepath)
         else:
             logging.warning("No data retrieved from the data source.")
     except Exception as e:
