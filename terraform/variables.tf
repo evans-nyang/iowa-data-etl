6ff088bd34c991d529dc2023527bf82cf2f17ec6
Johnevans Nyawanga <obuyanyawanga@students.ku.ac.ke>
2023-12-19 21:00:02 +0000

    Add variables.tf file

---
+++ terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "region" {
+  description = "The AWS region to deploy to"
+  type        = string
+}
+
+variable "bucket_name" {
+  description = "The name of the S3 bucket"
+  type        = string
+}
+
+variable "environment" {
+  description = "The environment to deploy to"
+  type        = string
+}
